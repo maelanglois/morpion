d33b71ce7b73c3adc91a354231deb92f4b6fabbc
maelanglois <150166864+maelanglois@users.noreply.github.com>
2025-01-14 12:31:04 +0100

    players turns

--- morpion/src/pages/Game.jsx
+++ morpion/src/pages/Game.jsx
@@ -15,16 +15,46 @@ function Square({ value, squareClick }) {
 }
 
 function Gameboard() {
+    const [xNext, setXNext] = useState(true);
     const[square, setSquare] = useState(Array(9).fill(null));
 
     function click(i) {
+        if(square[i] || Winner(square)) {
+            return;
+        }
+
         const next = square.slice();
-        next[i] = <img src={cross} className="played"/>;
+
+        if(xNext) {
+            next[i] = <img src={cross} className="played"/>;
+        } else {
+            next[i] = <img src={circle} className="played"/>;
+        }
+
         setSquare(next);
+        setXNext(!xNext);
+    }
+
+    const winner = Winner(square);
+    let status;
+    const turn = "turn";
+    if(winner) {
+        status = "Winner : " + winner;
+    } else {
+        status = (xNext ? <img src={cross} className="logoexample"/> : <img src={circle} className="logoexample"/>);
     }
 
     return (
         <>
+            <tr>
+                <td className='logos'>
+                    <img src={cross} className="logoexample"/> <img src={circle} className="logoexample"/>
+                </td>
+                <td className="turn">{status} {turn}</td>
+                <td className='refreshing'>
+                    <button className="refresh material-symbols-outlined">refresh</button>
+                </td>
+            </tr>
             <tr>
                 <td>
                     <Square value={square[0]} squareClick={() => click(0)}/>
@@ -62,20 +92,33 @@ function Gameboard() {
     )
 }
 
+function Winner(square) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for(let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (square[a] && square[a] === square[b] && square[a] === square[c]) {
+            return square[a];
+        }
+    }
+    return null;
+}
+
 function Game() {
     return(
         <>
             <div className="container">
                 <table className="gameboard" cellSpacing={20}>
-                    <tr>
-                        <td className='logos'>
-                            <img src={cross} className="logoexample"/> <img src={circle} className="logoexample"/>
-                        </td>
-                        <td className="turn"><img src={cross} className="logoexample"/> turn</td>
-                        <td className='refreshing'>
-                            <button className="refresh material-symbols-outlined">refresh</button>
-                        </td>
-                    </tr>
+                    
                     <Gameboard />
                     <tr>
                         <td className="case bg1">
