95e56dd2281c2d04e5431a1a43be60f2f258b132
maelanglois <150166864+maelanglois@users.noreply.github.com>
2025-01-14 10:37:06 +0100

    remplir les cases de croix au clic

--- morpion/src/pages/Game.jsx
+++ morpion/src/pages/Game.jsx
@@ -1,5 +1,66 @@
+import { useState } from 'react'
+
 import cross from '../components/img/cross.svg'
 import circle from '../components/img/circle.svg'
+
+function Square({ value, squareClick }) {
+
+    return(
+        <>
+            <button className="case" onClick={squareClick}>
+                {value}
+            </button>
+        </>
+    )
+}
+
+function Gameboard() {
+    const[square, setSquare] = useState(Array(9).fill(null));
+
+    function click(i) {
+        const next = square.slice();
+        next[i] = <img src={cross} className="played"/>;
+        setSquare(next);
+    }
+
+    return (
+        <>
+            <tr>
+                <td>
+                    <Square value={square[0]} squareClick={() => click(0)}/>
+                </td>
+                <td>
+                    <Square value={square[1]} squareClick={() => click(1)}/>
+                </td>
+                <td>
+                    <Square value={square[2]} squareClick={() => click(2)}/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <Square value={square[3]} squareClick={() => click(3)}/>
+                </td>
+                <td>
+                    <Square value={square[4]} squareClick={() => click(4)}/>
+                </td>
+                <td>
+                    <Square value={square[5]} squareClick={() => click(5)}/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <Square value={square[6]} squareClick={() => click(6)}/>
+                </td>
+                <td>
+                    <Square value={square[7]} squareClick={() => click(7)}/>
+                </td>
+                <td>
+                    <Square value={square[8]} squareClick={() => click(8)}/>
+                </td>
+            </tr>
+        </>
+    )
+}
 
 function Game() {
     return(
@@ -15,21 +76,7 @@ function Game() {
                             <button className="refresh material-symbols-outlined">refresh</button>
                         </td>
                     </tr>
-                    <tr>
-                        <td className="case"><img src={cross} className="played"/></td>
-                        <td className="case"><img src={circle} className="played"/></td>
-                        <td className="case"><img src={cross} className="played"/></td>
-                    </tr>
-                    <tr>
-                        <td className="case"><img src={circle} className="played"/></td>
-                        <td className="case"><img src={cross} className="played"/></td>
-                        <td className="case"><img src={cross} className="played"/></td>
-                    </tr>
-                    <tr>
-                        <td className="case"><img src={cross} className="played"/></td>
-                        <td className="case"><img src={cross} className="played"/></td>
-                        <td className="case"><img src={cross} className="played"/></td>
-                    </tr>
+                    <Gameboard />
                     <tr>
                         <td className="case bg1">
                             <player>x (P1)</player><br></br>
--- morpion/src/pages/Home.jsx
+++ morpion/src/pages/Home.jsx
@@ -1,15 +1,15 @@
 function Home() {
-return(
-    <>
-        <h1>Tic Tac <c1>Toe</c1></h1>
-        <h2>Gameplay mode</h2>
-        <div className="select">
-            <a href="">Classic tic-tac-toe</a>
-            <a href="">Three moves tic-tac-toe</a>
-            <a href="">Return to ongoing game</a>
-        </div>
-    </>
-)
+    return(
+        <>
+            <h1>Tic Tac <c1>Toe</c1></h1>
+            <h2>Gameplay mode</h2>
+            <div className="select">
+                <a href="">Classic tic-tac-toe</a>
+                <a href="">Three moves tic-tac-toe</a>
+                <a href="">Return to ongoing game</a>
+            </div>
+        </>
+    )
 }
 
 export default Home;
